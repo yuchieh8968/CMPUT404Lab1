c3aa7c6dc4467aded01e16fd37074ad66ed847a4
yuchieh8968 <yuchieh@ualberta.ca>
2023-01-09 00:29:03 -0700

    create first python script

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests, os
+
+# Q2 Returns version of the requests library installed
+print(requests.__version__)
+
+#Curl point 5. Get the google home page
+os.system('curl http://www.google.com')

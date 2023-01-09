06d63e4732fdbb5ea68d97d4ba3569cd0061c199
yuchieh8968 <yuchieh@ualberta.ca>
2023-01-09 00:34:44 -0700

    update python script

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ print(requests.__version__)
 
 #Curl point 5. Get the google home page
 os.system('curl http://www.google.com')
+
+#curl point 10. Python script that downloads itself
+os.system('curl -i https://raw.githubusercontent.com/yuchieh8968/CMPUT404Lab1/main/main.py')
